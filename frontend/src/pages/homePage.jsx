75274aaf08e7bd3986b0c213a4df60173fae8539
Rachit-007 <rachitpatel2106@gmail.com>
2025-08-04 21:25:32 -0400

    Added scroll bar css

--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -1,4 +1,8 @@
+import { useForm } from "react-hook-form";
+
 export const HomePage = () => {
+  const { register, handleSubmit } = useForm();
+
   return (
     <div>
       <div className="w-1/2 mx-auto">
@@ -12,18 +16,22 @@ export const HomePage = () => {
         </div>
       </div>
       <div className="flex justify-between gap-10 m-10">
-        <div className="mx-auto w-full h-72 shadow-md rounded-lg p-4 border-2 border-gray-200">
-          <textarea
-            className="border-0 outline-none focus:ring-0 bg-transparent w-full h-auto resize-none"
-            placeholder="Your text here..."
-          />
-        </div>
-        <div className="mx-auto w-full h-72 shadow-md rounded-lg p-4 border-2 border-gray-200">
-          <textarea
-            className="border-0 outline-none focus:ring-0 bg-transparent w-full resize-none"
-            placeholder="Your text here..."
-          />
-        </div>
+        <form className="mx-auto w-full h-96 shadow-md rounded-lg p-4 border-2 border-gray-200">
+          <div className=" flex flex-col justify-between h-full">
+            <div className="grow m-3 ">
+              <textarea
+                className="border-0 outline-none focus:ring-0 bg-transparent w-full h-full resize-none pr-3"
+                placeholder="Your text here..."
+              />
+            </div>
+            <div>
+              <button className="float-end p-2 m-1 bg-blue-400 rounded-4xl text-white ps-6 pe-6 hover:bg-blue-500 duration-500 font-semibold">
+                Simplify
+              </button>
+            </div>
+          </div>
+        </form>
+        <div className="mx-auto w-full h-96 shadow-md rounded-lg p-4 border-2 border-gray-200"></div>
       </div>
     </div>
   );
